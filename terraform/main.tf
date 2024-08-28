553719f46ed02752b832d41e4a57e239f0bf50ff
Deva <dvrharsha@gmail.com>
2024-08-28 13:59:36 +0530

    vpc updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,44 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+resource "aws_resourcegroups_group" "rg" {
+  name = "aws-tf-useast-rg-01"
+
+  resource_query {
+    query = <<JSON
+{
+  "ResourceTypeFilters": [
+    "AWS::EC2::Instance"
+  ],
+  "TagFilters": [
+    {
+      "Key": "Stage",
+      "Values": ["Test"]
+    }
+  ]
+}
+JSON
+  }
+
+  tags = {
+    created_by = "deva"
+  }
+}
+
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    created_by = "deva"
+  }
+}
+
+resource "aws_subnet" "sub1" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    created_by = "deva"
+  }
 }
